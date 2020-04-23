77470b1e5a2a99b4b507c6d73e2d9ad0e91ec5f0
Himanshu <hisuperuser@outlook.com>
2020-04-23 11:15:48 +0530

    outputmeth5code basic of print function

---
+++ Basic Python/outputmeth5code.py
@@ -0,0 +1,19 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Code for output like print method
+>>> # \n operator for text print in next line
+>>> print ("Jay \nHind")
+Jay 
+Hind
+>>> # print the multiple text string
+>>> # "Text 1" + "Text 2" + "Text 3"
+>>> print ("I Love" + "My" + "INDIA")
+I LoveMyINDIA
+>>> # improve the result
+>>> # using some space add in string
+>>> print ("I Love" + " My " + "INDIA")
+I Love My INDIA
+>>> # print the text multiple time
+>>> print ("Jay Hind "*3)
+Jay Hind Jay Hind Jay Hind 
+>>> 
