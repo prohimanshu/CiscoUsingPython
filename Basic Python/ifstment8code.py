70415ab49c3431288053daa8b4e7f7ad6bd5d30e
Himanshu <hisuperuser@outlook.com>
2020-04-23 12:39:19 +0530

    ifstment8code basic of python if stment

---
+++ Basic Python/ifstment8code.py
@@ -0,0 +1,15 @@
+# if (result of expression either True other False):
+# all code part of if statements
+if 12 > 10:
+	print ("12 greater then 10")
+# if statement is end after next line
+print ("if statement is end")
+
+# initialize a variable
+var = 20
+# inthis code if also part of if statement
+if var > 18:
+	print ("20 is greater then 18")
+	if var <= 30:
+		print ("20 is smaller then 30")
+print ("if statement is end")
