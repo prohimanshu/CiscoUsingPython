56537f0e9191a02e56c266c93f8b6c93d26bbe7a
Himanshu <hisuperuser@outlook.com>
2020-04-23 11:55:41 +0530

    operatorcomp7code Boolean and Comparisons

---
+++ Basic Python/operatorcomp7code.py
@@ -0,0 +1,35 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # initialize the value in variable
+>>> var = True
+>>> var
+True
+>>> # this is comparison of two value
+>>> 2 == 3
+False
+>>> "hello" == "hello"
+True
+>>> # unathor comparison operator is NOT(!=) invert the result
+>>> 1 != 1
+False
+>>> # 1 is not equal to 1 statement is False. invert the result
+>>> "Banana" != "Orange"
+True
+>>> # Again invert the result, both words is different
+>>> 20 != 10
+True
+>>> # 20 is not equal to 10 statement is true
+>>> # Now some new conditional operator
+>>> # > greater then
+>>> # < smaller then
+>>> # <= smaller then OR equal
+>>> # >= greater then OR equal
+>>> 12 > 8
+True
+>>> 10 < 10
+False
+>>> 7 <= 8
+True
+>>> 9 >= 9.0
+True
+>>> 
