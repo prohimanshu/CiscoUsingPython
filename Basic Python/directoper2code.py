a76a7cca29a3ec6b72f9110d23d4e73c55c2e8a6
Himanshu <hisuperuser@outlook.com>
2020-04-23 10:23:08 +0530

    directoper2code for direct operation

---
+++ Basic Python/directoper2code.py
@@ -0,0 +1,9 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Addition of code
+>>> 100 + 100
+200
+>>> # Subtraction of code
+>>> 200 - 100
+100
+>>> 
