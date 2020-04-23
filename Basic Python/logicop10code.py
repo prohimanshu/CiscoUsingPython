0f26e579b0123e26507fb2a619b6943942ae8465
Himanshu <hisuperuser@outlook.com>
2020-04-23 13:21:05 +0530

    logicop10code basic logic operation AND OR NOT

---
+++ Basic Python/logicop10code.py
@@ -0,0 +1,26 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # AND Operator Var AND Var works as Var × Var
+>>> 1 == 1 and 2 == 2
+True
+>>> 1 == 1 and 2 == 3
+False
+>>> 1 != 1 and 2 == 2
+False
+>>> 10 < 1 and 2 > 6
+False
+>>> # OR Operator Var OR Var works as Var + Var
+>>> 1 == 1 or 2 == 2
+True
+>>> 1 == 1 or 2 == 3
+True
+>>> 1 != 1 or 2 == 2
+True
+>>> 10 < 1 or 2 > 6
+False
+>>> # NOT Operator NOT Var works as invert the value
+>>> not 1 == 1
+False
+>>> not 1 > 7
+True
+>>> 
