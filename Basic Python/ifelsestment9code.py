62290ed16131e8ffbf6f7c58aa1ffa3e1a31f5b1
Himanshu <hisuperuser@outlook.com>
2020-04-23 12:51:50 +0530

    ifelsestment9code

---
+++ Basic Python/ifelsestment9code.py
@@ -0,0 +1,18 @@
+var = 50
+if var == 60:
+	print ("Yes if code execute")
+else:
+	print ("Yes else code execute")
+
+# initialize a variable
+var = 20
+# inthis code if also part of if statement
+if var == 10:
+	print ("var is 10")
+elif var == 20:
+	print ("var is 20")
+elif var == 30:
+	print ("var is 30")
+else:
+	print ("not found")
+print ("if statement is end")
