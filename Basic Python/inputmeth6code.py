e868163a96af0d43476516ed32e3f2d631900e64
Himanshu <hisuperuser@outlook.com>
2020-04-23 11:55:03 +0530

    inputmeth6code study for basic input method in python

---
+++ Basic Python/inputmeth6code.py
@@ -0,0 +1,19 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # code for input method
+>>> # take the direct input
+>>> input ("enter some text:")
+enter some text:hello UConfigIt
+'hello UConfigIt'
+>>> # code for add two number
+>>> # use float data type because user can enter anything
+>>> float (input ("enter number A:")) + float (input ("enter number B:"))
+enter number A:12.17
+enter number B:12
+24.17
+>>> # initialize the value in variable
+>>> var = input ("enter the number:")
+enter the number:3.14
+>>> print (var)
+3.14
+>>> 
