fbd7371a16135389525efaf3bc0935761122cb57
Himanshu <hisuperuser@outlook.com>
2020-05-12 21:52:41 +0530

    findnum14code while if else break keyword

---
+++ Basic Python/findnum14code.py
@@ -0,0 +1,12 @@
+i = 0
+while True:
+   i = i +1
+   if i == 2:
+      print("now i is equal to 2")
+      continue
+   if i == 5:
+      print("break statement")
+      break
+   print(i)
+
+print("end")
